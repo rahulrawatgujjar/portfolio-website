74a76e8a4831593f6a8c141f94b2f697f2a180af
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-29 16:11:54 +0530

    created footer, added buttons, fixed navbar

---
+++ static/script.js
@@ -0,0 +1,10 @@
+const resumeBtn= document.querySelector("#resume-btn");
+const githubBtn= document.querySelector("#github-btn");
+
+resumeBtn.addEventListener("click",()=>{
+  window.open("/static/resume.pdf","_blank");
+});
+
+githubBtn.addEventListener("click",()=>{
+  window.open('https://github.com/rahulrawatgujjar', '_blank');
+});
