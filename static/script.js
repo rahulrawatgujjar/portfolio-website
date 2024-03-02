ec7dce1b29f6c41623c49ae87478236667dc2c78
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-02 18:49:51 +0530

    make website responsive and also templates

--- static/script.js
+++ static/script.js
@@ -5,6 +5,12 @@ const linkedinDiv= document.querySelector(".linkedin");
 const emailDiv= document.querySelector(".email");
 const instaDiv= document.querySelector(".insta");
 const xTwitterDiv= document.querySelector(".x-twitter");
+const homeBtn= document.querySelector("#home-btn");
+const projectsBtn= document.querySelector("#projects-btn");
+const skillsBtn= document.querySelector("#skills-btn");
+const eduBtn= document.querySelector("#edu-btn");
+const footBtn= document.querySelector("#foot-btn");
+
 
 resumeBtn.addEventListener("click",()=>{
   window.open("https://mega.nz/folder/6Zc1USDL#8uc-HhPIM0Ky-N-ACc-ZRA","_blank");
@@ -34,3 +40,45 @@ instaDiv.addEventListener("click",()=>{
 xTwitterDiv.addEventListener("click",()=>{
   window.open("https://x.com/RahulRa02228239","_blank");
 });
+
+homeBtn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  scrollToSection(evt.target);
+})
+
+projectsBtn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  scrollToSection(evt.target);
+})
+
+eduBtn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  scrollToSection(evt.target);
+})
+
+skillsBtn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  scrollToSection(evt.target);
+})
+
+footBtn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  scrollToSection(evt.target);
+})
+
+
+
+
+function scrollToSection(target){
+  const section= document.querySelector(target.getAttribute("scroll-to"));
+  let offset=100;
+  if (section.tagName=="body" || section.id=="foot"){
+    offset=0;
+  }
+
+  const sectionPosition= section.offsetTop - offset;
+  window.scrollTo({
+    top: sectionPosition,
+    behavior: "smooth"
+  });
+}
